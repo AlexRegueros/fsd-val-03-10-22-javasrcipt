e7f0941da14a310fbe5d0b35f2ccd85e955a20fe
Alejandro Regueros <alex.regue81@gmail.com>
2022-10-03 13:57:44 +0200

    finaldeclase

---
+++ js/main.js
@@ -0,0 +1,70 @@
+
+// var academia = "Geeks Hub"
+
+// console.log(academia)
+
+// var edad = 30
+
+// console.log(edad)
+
+
+// var experiencia = true
+
+// console.log(experiencia)
+
+
+
+
+// var numero1 = 50;
+
+// var numero2 = 73;
+
+// var resultado = numero1 + numero2;
+
+// console.log(resultado);
+
+
+// var edad = 25;
+
+// if( edad >= 18){
+//  console.log("puedes ver la pelicula")
+
+// }else{
+//     console.log("no puedes verla")
+
+// }
+// ()
+
+var mes ="agosto";
+
+switch(temporada){
+
+    case "Diciembre":
+    case "Enero":
+    case "Febrero":
+        console.log("Estamos en invierno");
+        break;
+    
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+        console.log("Estamos en Primavera");
+        break;
+
+    case "Junio":
+    case "Julio":
+    case "Agosto":
+        console.log("Estamos en Verano");
+        break;
+
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+        console.log("Estamos en Otoño");
+        break;
+}
+
+
+
+
+
