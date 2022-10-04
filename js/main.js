924e21618f52b08af584b0adecfec94959497e02
Alejandro Regueros <alex.regue81@gmail.com>
2022-10-04 09:35:51 +0200

    3ºejercico

--- js/main.js
+++ js/main.js
@@ -35,36 +35,65 @@
 // }
 // ()
 
-var mes ="agosto";
+// var mes ="agosto";
 
-switch(temporada){
+// switch(temporada){
 
-    case "Diciembre":
-    case "Enero":
-    case "Febrero":
-        console.log("Estamos en invierno");
-        break;
+//     case "Diciembre":
+//     case "Enero":
+//     case "Febrero":
+//         console.log("Estamos en invierno");
+//         break;
     
-    case "Marzo":
-    case "Abril":
-    case "Mayo":
-        console.log("Estamos en Primavera");
-        break;
+//     case "Marzo":
+//     case "Abril":
+//     case "Mayo":
+//         console.log("Estamos en Primavera");
+//         break;
 
-    case "Junio":
-    case "Julio":
-    case "Agosto":
-        console.log("Estamos en Verano");
-        break;
+//     case "Junio":
+//     case "Julio":
+//     case "Agosto":
+//         console.log("Estamos en Verano");
+//         break;
 
-    case "Septiembre":
-    case "Octubre":
-    case "Noviembre":
-        console.log("Estamos en Otoño");
-        break;
+//     case "Septiembre":
+//     case "Octubre":
+//     case "Noviembre":
+//         console.log("Estamos en Otoño");
+//         break;
+// }
+
+// var nombre = "Manuel";
+// var edad = 26;
+
+// if((nombre == "Jesus") && (edad == 26)){
+//     console.log("Que bien me cae este chico");
+    
+    
+// }else{
+//     console.log("me cae fatal");
+// }
+
+
+
+
+//  var numero1 = 10;
+//  var numero2 = 20;
+//  var resultado = numero1 > numero2
+
+//  console.log();
+
+
+// var nombre = "Alejandro";
+
+// if(nombre== "Alejandro"){
+//     console.log("Bienvenido Alejandro");
+// }
+
+
+var nombre = prompt ("¿Cuál es tu nombre?")
+
+if(nombre== "Alex"){
+    console.log("Bienvenido Ana");
 }
-
-
-
-
-
